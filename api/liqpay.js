d53f812d5f09400cc0b8f148a32a254844d73680
slavik <151384840+slavon4ikk3kk@users.noreply.github.com>
2025-08-12 13:53:31 +0300

    Update liqpay.js

--- api/liqpay.js
+++ api/liqpay.js
@@ -17,7 +17,7 @@ export default function handler(req, res) {
         currency: 'UAH',
         description,
         order_id,
-        result_url: "http://localhost:3000/payment-result",
+        result_url: "http://localhost:3000/",
         sandbox: 1 // 1 = тестовий режим, 0 = бойовий
     };
 

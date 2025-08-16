c4098ebbd8e58a51dd8a78cb5ed32de073245088
slavik <151384840+slavon4ikk3kk@users.noreply.github.com>
2025-08-16 12:43:37 +0300

    Update liqpay.js

--- api/liqpay.js
+++ api/liqpay.js
@@ -1,13 +1,22 @@
 import crypto from "crypto";
 
 export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+
   const { amount, description } = req.query;
 
   const public_key = process.env.LIQPAY_PUBLIC_KEY;
   const private_key = process.env.LIQPAY_PRIVATE_KEY;
 
-  // тут завжди унікальний order_id
-  const order_id = "order_" + Date.now() + "_" + Math.floor(Math.random() * 100000);
+  const order_id = "order_" + Date.now() + "_" + Math.floor(Math.random() * 1000);
 
   const payload = {
     public_key,
@@ -21,11 +30,10 @@ export default function handler(req, res) {
   };
 
   const base64Data = Buffer.from(JSON.stringify(payload)).toString("base64");
-
   const signature = crypto
     .createHash("sha1")
     .update(private_key + base64Data + private_key)
     .digest("base64");
 
-  res.status(200).json({ data: base64Data, signature });
+  res.status(200).json({ data: base64Data, signature, order_id });
 }

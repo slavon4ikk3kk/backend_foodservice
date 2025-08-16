45b8d74a80753a7856e99a3532bdad2d47493be2
slavik <151384840+slavon4ikk3kk@users.noreply.github.com>
2025-08-16 12:02:32 +0300

    add new order_id

--- api/liqpay.js
+++ api/liqpay.js
@@ -4,11 +4,13 @@ export default function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    const { amount, description, order_id } = req.query;
+    const { amount, description} = req.query;
 
     const public_key = process.env.LIQPAY_PUBLIC_KEY;
     const private_key = process.env.LIQPAY_PRIVATE_KEY;
 
+    const order_id = `order_${Date.now()}_${Math.floor(Math.random() * 100000)}`;
+    
     const data = {
         public_key,
         version: 3,

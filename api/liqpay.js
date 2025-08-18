9ba7fdf305d822416a20ce4267e88e3c45a6a82c
slavik <151384840+slavon4ikk3kk@users.noreply.github.com>
2025-08-18 22:32:19 +0300

    Update liqpay.js

--- api/liqpay.js
+++ api/liqpay.js
@@ -23,7 +23,7 @@ export default function handler(req, res) {
         description,
         order_id,
         result_url: "http://localhost:3000/payment-result", // куди повернеться користувач
-        server_url: "http://localhost:3000/api/liqpay-callback", // куди LiqPay надішле статус
+        server_url: "https://backend-foodservice.vercel.app/api/liqpay-callback", // куди LiqPay надішле статус
         sandbox: 1 // 1 = тестовий режим
     };
 
